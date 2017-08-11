89882ede54da8df774bea642c2f600ad5d593ceb
Andrew Teesdale <ateesdalejr@users.noreply.github.com>
2017-08-10 20:29:33 -0700

    Added nice rainbow thing.

---
+++ main.js
@@ -0,0 +1,14 @@
+var colors=[
+"#FF9999",
+"#FFD499",
+"#FFFF99",
+"#99FFBB",
+"#99B2FF",
+"#CC99FF",
+];
+window.addEventListener("load",function() {
+  var backing=document.getElementById("backing");
+  for (var i=0;i<6;i++) {
+    backing.innerHTML+=('<div class="back" style="background-color: '+colors[i]+';"></div>');
+  }
+});
